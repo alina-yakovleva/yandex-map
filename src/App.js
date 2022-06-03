367157f5c429d9a7dac7926c9427c67e02d4f4ac
Alina Yakovleva <alinayakovleva446@gmail.com>
2022-06-03 23:40:36 +0300

    правки

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ import data from "./data.json";
 import { getUniqCities, getUniqMarkers, SEPARATOR } from "./utils";
 
 const preparedData = getUniqCities(data);
+
+preparedData.sort(function (a, b) {
+  if (a.CITY < b.CITY) return -1;
+  if (a.CITY > b.CITY) return 1;
+  return 0;
+});
+
 const markers = getUniqMarkers(data);
 
 function App() {
   const [coordinates, setCoordinates] = useState([55.751244, 37.618423]);
   const [zoom, setZoom] = useState(5);
-
-  preparedData.sort(function (a, b) {
-    if (a.CITY < b.CITY) return -1;
-    if (a.CITY > b.CITY) return 1;
-    return 0;
-  });
 
   const handleChange = (event) => {
     const coordinates = event.target.value.split(SEPARATOR).map(Number);
@@ -29,10 +30,13 @@ function App() {
       <div className="container">
         <div className="select">
           <h3 style={{ textAlign: "center", margin: "10px" }} variant="h4">
-            Города
+            Магазины
           </h3>
 
           <select value={coordinates.join(SEPARATOR)} onChange={handleChange}>
+            <option value={[55.751244, 37.618423].join(SEPARATOR)} disabled>
+              Выберите город
+            </option>
             {preparedData.map((obj) => (
               <option key={obj.CITY} value={[obj.LAT, obj.LNG].join(SEPARATOR)}>
                 {obj.CITY}
@@ -40,23 +44,26 @@ function App() {
             ))}
           </select>
         </div>
-        <YMaps>
-          <Map
-            width="1000px"
-            height="600px"
-            state={{ center: coordinates, zoom }}
-          >
-            {markers.map((marker) => (
-              <Placemark
-                options={{
-                  preset: "islands#blueDotIcon",
-                }}
-                key={[marker.LAT, marker.LNG].join(SEPARATOR)}
-                geometry={[marker.LAT, marker.LNG]}
-              />
-            ))}
-          </Map>
-        </YMaps>
+        <div className="map">
+          <h2 className="loader">Загрузка карты...</h2>
+          <YMaps>
+            <Map
+              width="100%"
+              height="100%"
+              state={{ center: coordinates, zoom }}
+            >
+              {markers.map((marker) => (
+                <Placemark
+                  options={{
+                    preset: "islands#blueDotIcon",
+                  }}
+                  key={[marker.LAT, marker.LNG].join(SEPARATOR)}
+                  geometry={[marker.LAT, marker.LNG]}
+                />
+              ))}
+            </Map>
+          </YMaps>
+        </div>
       </div>
     </div>
   );
